c5e1186431ee1342033e9910042c6efd6a28cd68
Malik F. Hayat <fasil.hayat@gmail.com>
2023-10-24 21:02:32 +0200

    TODO: Read different configuration settings.

--- Infrastructure/ServiceFactory.cs
+++ Infrastructure/ServiceFactory.cs
@@ -19,29 +19,57 @@ public sealed class ServiceFactory<TService> : IServiceFactory<TService>
         _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="parameters"></param>
+    /// <returns></returns>
     public async Task<TResult?> Execute<TResult>(IEnumerable<KeyValuePair<string, string>> parameters) where TResult : class, new()
     {
-        //TODO: Read configuration
-        var settings = _configuration.GetSection($"{typeof(TService).Name}Config");
         var querystring = $"/?{string.Join("&", parameters.Select(x => $"{x.Key}={x.Value}"))}";
+
+        //TODO: Read configuration
+        var someting = ExtractServiceConfiguration();
+        
         var httpClient = _httpClientFactory.CreateClient($"{typeof(TService).Name}Client");
         return await GetData<TResult>(httpClient, querystring);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="querystring"></param>
+    /// <returns></returns>
     public async Task<TResult?> Execute<TResult>(string querystring) where TResult : class, new()
     {
         //TODO: Read configuration
-        var settings = _configuration.GetSection($"{typeof(TService).Name}Config");
+        var someting = ExtractServiceConfiguration();
+
         var httpClient = _httpClientFactory.CreateClient($"{typeof(TService).Name}Client");
         return await GetData<TResult>(httpClient, querystring);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="json"></param>
+    /// <returns></returns>
     public async Task<TResult?> Execute<TResult>(JsonObject json) where TResult : class, new()
     {
         var httpClient = _httpClientFactory.CreateClient($"{typeof(TService).Name}Client");
         return await PostData<TResult>(httpClient, json);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="client"></param>
+    /// <param name="parameters"></param>
+    /// <returns></returns>
     private async Task<TResult?> GetData<TResult>(HttpClient client, string parameters) where TResult : class, new()
     {
         var uri = new Uri($"{client.BaseAddress}{parameters}");
@@ -53,6 +81,13 @@ public sealed class ServiceFactory<TService> : IServiceFactory<TService>
         return result;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="client"></param>
+    /// <param name="json"></param>
+    /// <returns></returns>
     private async Task<TResult?> PostData<TResult>(HttpClient client, JsonObject json) where TResult : class, new()
     {
         var content = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
@@ -62,5 +97,47 @@ public sealed class ServiceFactory<TService> : IServiceFactory<TService>
         var stream = await response.Content.ReadAsStreamAsync();
         var result = await JsonSerializer.DeserializeAsync<TResult>(stream, _options);
         return result;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public IConfigurationSection ExtractServiceConfiguration()
+    {
+        var configSections = new List<Func<IConfigurationSection>>
+        {
+            JwtConfiguration,
+            ApiKeyConfiguration
+        };
+
+        var configSection = configSections.Select(x => x.Invoke());
+        return configSection.FirstOrDefault()!;
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private IConfigurationSection JwtConfiguration()
+    {
+        //TODO: Read configuration
+        var settings = _configuration.GetSection($"{typeof(TService).Name}Config");
+        var setting = settings.GetSection("JwtConfig").Value;
+
+        return settings;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private IConfigurationSection ApiKeyConfiguration()
+    {
+        //TODO: Read configuration
+        var settings = _configuration.GetSection($"{typeof(TService).Name}Config");
+
+        return settings;
     }
 }
