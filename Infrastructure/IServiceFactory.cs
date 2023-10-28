355f2b22b0a0081c023220c28022c7406d59683d
Fasil Malik Hayat <fasil.hayat@gmail.com>
2023-10-28 11:13:12 +0200

    Refactored

--- Infrastructure/IServiceFactory.cs
+++ Infrastructure/IServiceFactory.cs
@@ -1,31 +1,34 @@
 ﻿using System.Text.Json.Nodes;
 
-namespace Corporate.Application.Services.Infrastructure
+namespace Corporate.Application.Services.Infrastructure;
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="TService"></typeparam>
+public interface IServiceFactory<TService>
 {
-    public interface IServiceFactory<TService>
-    {
-        /// <summary>
-        /// Associated with HTTP GET method.
-        /// </summary>
-        /// <typeparam name="TResult"></typeparam>
-        /// <param name="parameters"></param>
-        /// <returns></returns>
-        Task<TResult?> Execute<TResult>(IEnumerable<KeyValuePair<string, string>> parameters) where TResult : class;
+    /// <summary>
+    /// Associated with HTTP GET method.
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="parameters"></param>
+    /// <returns></returns>
+    Task<TResult?> Execute<TResult>(IEnumerable<KeyValuePair<string, string>> parameters) where TResult : class;
 
-        /// <summary>
-        /// Associated with HTTP GET method.
-        /// </summary>
-        /// <typeparam name="TResult"></typeparam>
-        /// <param name="querystring"></param>
-        /// <returns></returns>
-        Task<TResult?> Execute<TResult>(string querystring) where TResult : class;
+    /// <summary>
+    /// Associated with HTTP GET method.
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="querystring"></param>
+    /// <returns></returns>
+    Task<TResult?> Execute<TResult>(string querystring) where TResult : class;
 
-        /// <summary>
-        /// Associated with HTTP POST method.
-        /// </summary>
-        /// <typeparam name="TResult"></typeparam>
-        /// <param name="json"></param>
-        /// <returns></returns>
-        Task<TResult?> Execute<TResult>(JsonObject json) where TResult : class;
-    }
+    /// <summary>
+    /// Associated with HTTP POST method.
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="json"></param>
+    /// <returns></returns>
+    Task<TResult?> Execute<TResult>(JsonObject json) where TResult : class;
 }
